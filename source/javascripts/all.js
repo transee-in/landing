9f48a36634b92fcb00e30a0813d01b2b87615fd7
Yuri Artemev <i@artemeff.com>
2015-04-17 11:43:01 +0300

    add initial app presentation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,21 @@
 //= require_tree .
+
+ready(function() {
+  var timer = setInterval(nextImage, 5000);
+  var curImage = 1;
+  var numImages = 3;
+
+  function nextImage() {
+    var slide = document.getElementsByClassName('slide-' + curImage)[0];
+
+    slide.classList.remove('active');
+
+    curImage++;
+    if (curImage > numImages) {
+      curImage = 1;
+    }
+
+    slide = document.getElementsByClassName('slide-' + curImage)[0];
+    slide.classList.add('active');
+  }
+});
